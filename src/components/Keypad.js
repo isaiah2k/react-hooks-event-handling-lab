71ea98dc5c3ea5bb58f847408be2270622fffdb9
Isaiah <zaymgmt707@gmail.com>
2024-12-17 18:25:55 -0800

    updates components

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,12 @@
 // Code Keypad Component Here
-
+const handleChange = () => console.log('Entering password...')
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input
+            type="password"
+            onChange={handleChange}/>
+        </div>
     )
 }
 
